78ea5ef1c121fcfee04696b9cc99332c55aa1d1d
Hakan Raberg <hraberg@yahoo.se>
2012-02-04 11:34:12 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject shen-clj "0.1.0-SNAPSHOT"
+  :description "Shen is a portable functional programming language"
+  :license {:name "Shen License"
+            :url "http://www.shenlanguage.org/license.html"}
+  :source-path ["src/clj" "src/ki"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta1"]])
