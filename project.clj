7a8d37f8ab10070c1923ba9f198fa198832036ee
Hakan Raberg <hraberg@yahoo.se>
2012-02-04 11:37:16 +0100

    Moving ki to classpath

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Shen is a portable functional programming language"
   :license {:name "Shen License"
             :url "http://www.shenlanguage.org/license.html"}
-  :source-path ["src/clj" "src/ki"]
+  :source-path "src/clj"
+  :extra-classpath-dirs ["src/ki"]
   :dependencies [[org.clojure/clojure "1.4.0-beta1"]])
