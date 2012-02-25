4c66972a8ce86ba7f7be83d36957c9e5541d7536
Hakan Raberg <hraberg@yahoo.se>
2012-02-25 12:43:33 +0000

    Playing better with clojure.test

--- test/shen/test.clj
+++ test/shen/test.clj
@@ -4,5 +4,5 @@
             [shen.primitives]))
 
 (deftest README.shen
-  (shen/read-file "shen/test-programs/README.shen")
+  (is (shen/read-file "shen/test-programs/README.shen"))
   (is (zero? (shen.primitives/value '*failed*))))
