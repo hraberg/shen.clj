e5d262294d86648608d3369f21c82ad9569bfe44
Hakan Raberg <hraberg@yahoo.se>
2012-03-06 18:58:09 +0000

    Update to Marginalia 0.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.shenlanguage.org/license.html"}
   :dependencies [[org.clojure/clojure "1.4.0-beta1"]]
   :dev-dependencies [[org.clojure/tools.trace "0.7.2-SNAPSHOT"]
-                     [marginalia "0.7.0-SNAPSHOT"]
+                     [marginalia "0.7.0"]
                      [lein-difftest "1.3.7"]]
   :repositories [["sonatype snapshots"
                   "https://oss.sonatype.org/content/repositories/snapshots/"]]
