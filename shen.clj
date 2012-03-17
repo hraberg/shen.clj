a24882c111f7e0d3fb8b791728c2717a71d902d3
Hakan Raberg <hraberg@yahoo.se>
2012-03-17 13:18:37 +0000

    optional rlwrap

--- shen.clj
+++ shen.clj
@@ -3,11 +3,13 @@
 version=1.4.0-beta5
 clojure=clojure-$version.jar
 repo=http://repo1.maven.org/maven2
+rlwrap=$(which rlwrap2) || ""
+java="$rlwrap java -Xss4m $JAVA_OPTS"
 
 if [ -e  shen.clj-*-standalone.jar ]; then
-	java -Xss4m -jar shen.clj-*-standalone.jar
+	$java -jar shen.clj-*-standalone.jar
 else
 	mkdir -p classes lib
 	test -e lib/$clojure || curl $repo/org/clojure/clojure/$version/$clojure > lib/$clojure
-	java -cp lib/$clojure:src:classes clojure.main -m shen.install
+	$java -cp lib/$clojure:src:classes clojure.main -m shen.install
 fi
