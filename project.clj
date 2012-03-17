af72d46c186155f84a5bbbdb222ef9824d5357d9
Hakan Raberg <hraberg@yahoo.se>
2012-03-17 12:21:09 +0000

    Guess this should be another version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shen.clj "0.1.2"
+(defproject shen.clj "0.1.3-SNAPSHOT"
   :description "Shen is a portable functional programming language by Mark Tarver"
   :license {:name "Shen License"
             :url "http://www.shenlanguage.org/license.html"}
