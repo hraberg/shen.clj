28544b259ccdaa55ca46ddb87b8b6c637e1d7473
Hakan Raberg <hraberg@yahoo.se>
2012-04-17 20:01:37 +0100

    upgrading to Clojure 1.4

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject shen.clj "0.1.4"
+(defproject shen.clj "0.1.6-SNAPSHOT"
   :description "Shen is a portable functional programming language by Mark Tarver"
   :license {:name "Shen License"
             :url "http://www.shenlanguage.org/license.html"}
   :url "https://github.com/hraberg/shen.clj"
-  :dependencies [[clojure "1.4.0-beta5"]]
-  :dev-dependencies [[org.clojure/tools.trace "0.7.2-20120223.025622-2"]
-                     [marginalia "0.7.0"]
+  :dependencies [[clojure "1.4.0"]]
+  :dev-dependencies [[marginalia "0.7.0"]
                      [lein-difftest "1.3.7"]]
   :repositories [["sonatype snapshots"
                   "https://oss.sonatype.org/content/repositories/snapshots/"]]
